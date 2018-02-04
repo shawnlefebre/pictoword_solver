39a6f7c4cf35af5ecdb9201df48aa146ef43fdd5
shawnlefebre <shmebula@yahoo.com>
2018-02-03 21:22:59 -0500

    Typos in readme

--- pictoword_solver.py
+++ pictoword_solver.py
@@ -12,12 +12,12 @@ import os
 
 word_dicts = [
     '/usr/share/dict/words',  # Included with Linux distros
-    '3esl.txt',  # 23k words From http://wordlist.aspell.net/12dicts/
-    '6of12.txt',  # 34k words From http://wordlist.aspell.net/12dicts/
-    '2of12.txt',  # 41k words From http://wordlist.aspell.net/12dicts/
-    '2of12inf.txt',  # 82k words From http://wordlist.aspell.net/12dicts/
-    'en_US.dic',  # 49k words Didn't have people
-    'words.txt',  # 467k words From https://github.com/dwyl/english-words
+    '3esl.txt',  # 23k words from http://wordlist.aspell.net/12dicts/
+    '6of12.txt',  # 34k words from http://wordlist.aspell.net/12dicts/
+    '2of12.txt',  # 41k words from http://wordlist.aspell.net/12dicts/
+    '2of12inf.txt',  # 82k words from http://wordlist.aspell.net/12dicts/
+    'en_US.dic',  # 49k words from https://sourceforge.net/projects/hunspell/files/Spelling%20dictionaries/en_US/
+    'words.txt',  # 467k words from https://github.com/dwyl/english-words
 ]
 
 
